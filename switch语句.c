97df5fd9157b89ae080529d17e7d89b163fc4b1d
recluse-c <125347793+recluse-c@users.noreply.github.com>
2023-02-14 19:28:21 +0800

    Add files via upload

---
+++ switch语句.c
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+int main()
+{
+	int n;
+	printf("请输入一个1~7的数\n");
+	scanf_s("%d", &n);
+	switch (n)
+	{
+	case 1:
+		printf("星期一\n");
+		break;
+	case 2:
+		printf("星期二\n");
+		break;
+	case 3:
+		printf("星期三\n");
+		break;
+	case 4:
+		printf("星期四\n");
+		break;
+	case 5:
+		printf("星期五\n");
+		break;
+	case 6:
+		printf("星期六\n");
+		break;
+	case 7:
+		printf("星期日\n");
+		break;
+	default:
+		printf("输入有误\n");
+		break;
+	}
+	return 0;
+}
